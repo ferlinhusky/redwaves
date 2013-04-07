a36196d7f7530c8967c0f93bc6aefd2b2bc129a3
Ferlin Husky <kingmountain@gmail.com>
2013-04-06 21:51:25 -0400

    Added some weapons
    Nothing big. Slow day.

--- js/classes/Weapons.js
+++ js/classes/Weapons.js
@@ -15,13 +15,13 @@ var Sword = Weapon.extend({
 });
 
 // Long sword
-var longsword = Sword.extend({ init: function(){ this._super("Long Sword", "longsword", "1d6", "steel", false); } });
+var longsword = Sword.extend({ init: function(){ this._super("Long sword", "longsword", "1d6", "steel", false); } });
 
 // Short sword
-var shortsword = Sword.extend({ init: function(){ this._super("Short Sword", "shortsword", "1d4", "steel", false); } });
+var shortsword = Sword.extend({ init: function(){ this._super("Short sword", "shortsword", "1d4", "steel", false); } });
 
 // Broad sword
-var broadsword = Sword.extend({ init: function(){ this._super("Broad Sword", "broadsword", "1d10", "steel", true); } });
+var broadsword = Sword.extend({ init: function(){ this._super("Broad sword", "broadsword", "1d10", "steel", true); } });
 
 // Staff subclass
 var Staff = Weapon.extend({
@@ -31,7 +31,7 @@ var Staff = Weapon.extend({
 });
 
 // Wooden Staff
-var woodenstaff = Staff.extend({ init: function(){ this._super("Wooden Staff", "woodenstaff", "1d2", "wood"); } });
+var woodenstaff = Staff.extend({ init: function(){ this._super("Wooden staff", "woodenstaff", "1d2", "wood"); } });
 
 
 // Appendage subclass
@@ -62,8 +62,14 @@ var Firearm = Weapon.extend({
 	}
 });
 
+// Crossbow
+var crossbow = Firearm.extend({ init: function(){ this._super("Crossbow", "crossbow", 3, 3, 3, "wood", true); } });
+
+// English Longbow
+var englishlongbow = Firearm.extend({ init: function(){ this._super("English longbow", "englishlongbow", 3, 1, 5, "steel", true); } });
+
 // Shotgun
 var shotgun = Firearm.extend({ init: function(){ this._super("Shotgun", "shotgun", 6, 3, 4, "steel", true); } });
 
 // Six shooter
-var sixshooter = Firearm.extend({ init: function(){ this._super("Six shooter", "sixshooter", 3, 1, 3, "steel", true); } });
+var sixshooter = Firearm.extend({ init: function(){ this._super("Six shooter", "sixshooter", 4, 1, 6, "steel", true); } });
