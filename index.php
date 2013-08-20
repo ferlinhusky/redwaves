a87a546b373541ce96d0efa9286930d311caf166
Ferlin Husky <kingmountain@gmail.com>
2013-08-20 00:10:46 -0400

    Updated Help

--- index.php
+++ index.php
@@ -28,105 +28,115 @@
 	<!-- Help content -->
 	<table id="help"><tr><td class="margin">&nbsp;</td><td class="content">
 		<h1>How to play</h1>
+		
 		<h2>Overview</h2>
-			<p><b>Over red waves of sand</b> is a single-player adventure game in a semi-ASCII style. If
-			you've every played D&D or any adventure game of any sort you're probably familiar with
-			the player types and stats being used here. If not, isn't learning all about discovery?</p>
-			
-			<p>The long and short of it is that you and your companions are going on a series of
-			@dventures&trade; wherein you kill all manner of monster. Why? Does the caged bird know
-			why it sings? Exactly. Nobody does. Except you, and that is why you must fight.</p>
+		<p><b>Over red waves of sand</b> is a single-player adventure game in a semi-ASCII style. If
+		you've every played D&D or any adventure game of any sort you're probably familiar with
+		the player types and stats being used here. If not, isn't learning all about discovery?</p>
+		
+		<p>The long and short of it is that you and your companions are going on a series of
+		@dventures&trade; wherein you kill all manner of monster. Why? Does the caged bird know
+		why it sings? Exactly. Nobody does. Except you, and that is why you must fight.</p>
+		
 		<h3>Attention mobile users</h3>
-			<p>So I'm working to make this whole thing cool for phones and tablets and it <i>mostly is</i>
-			for tablets and minis, but not so much on phones. It'll look fine on iOS Safari in portrait mode,
-			but not on mobile Chrome so much and not in landscape mode. Fixes for that are upcoming.</p>
+		<p>I've tested this for the iPhone 4/5, iPad, iPad mini and various Android tablets (thanks,
+		Target demo models!) and it works pretty well on all of those. The only weird one is Chrome for iOS, so
+		if you're using that right now, you may want to switch.</p>
+		
 		<h2>Player stats and skills</h2>
-			<p>All characters have the following attributes: Strength, Dexterity, Constitution, Intelligence,
-			Wisdom, and Charisma. In addition, all characters have hit points (HP), action points (AP),
-			experience points (XP), an armor class (AC), and a character level (Lvl).</p>
+		<p>All characters have the following attributes: Strength, Dexterity, Constitution, Intelligence,
+		Wisdom, and Charisma. In addition, all characters have hit points (HP), action points (AP),
+		experience points (XP), an armor class (AC), and a character level (Lvl).</p>
+		
+		<h3>In summation</h3>
+		<p>You lose HP when you get hit; zero hit points means your character is dead. But don't fret,
+		they'll be back for the next @dventure&trade;, they just won't gain any XP at the end of the
+		level. Action points determine how much you can do during your turn (moving, fighting, spellcasting).
+		Experience points are gained at the end of each @dventure&trade; for a variety of reasons. Gain
+		enough XP and your character will gain levels.</p>
+		
+		<p>Gaining levels means raising your attribute scores, which in turn raises your HP, AP, along
+		with all kinds of other nice advantages. Armor class is lowered (lower is better) by
+		wearing stronger armor and having high Dexterity. It's basically a measure of how hard it is
+		(or isn't) to hit you. It is so hard to not hit you right now.</p>
 			
-			<h3>In summation</h3>
-			<p>You lose HP when you get hit; zero hit points means your character is dead. But don't fret,
-			they'll be back for the next @dventure&trade;, they just won't gain any XP at the end of the
-			level. Action points determine how much you can do during your turn (moving, fighting, spellcasting).
-			Experience points are gained at the end of each @dventure&trade; for a variety of reasons. Gain
-			enough XP and your character will gain levels.</p>
-			
-			<p>Gaining levels means raising your attribute scores, which in turn raises your HP, AP, along
-			with all kinds of other nice advantages. Armor class is lowered (lower is better) by
-			wearing stronger armor and having high Dexterity. It's basically a measure of how hard it is
-			(or isn't) to hit you. It is so hard to not hit you right now.</p>
 		<h2>Player character (PC) types</h2>
-			<p>There are 8 available player types: Fighter, Knight, Wizard, Thief, Wolfman, Harpy, Young
-			Priest, and Old Priest. In the descriptions I'll be mixing him/her/he/she/etc. but for all
-			characters (except for the Wolfman and Harpy) genders are randomly assigned in the game.</p>
-			
-			<p>Individual skills are highlighted in white. You probably don't know what they do, but I
-			bet you can guess in an educated way.</p>
-			<h3>Fighter</h3>
-			<p>The Fighter, as you may have guessed, likes to fight. He comes equipped with a long sword
-			and a crossbow. He is both a <span class="skill">marksman</span> and <span class="skill">tenacious</span>.
-			Marksmanship gives the
-			fighter a better chance of hitting his target with any ranged weapon; tenacity allows the
-			Fighter to overcome certain death (50% of the time) and regain hit points after what would have
-			been a fatal blow.</p>
-			<h3>Knight</h3>
-			<p>The Knight is a walking, talking (always talking) medieval tank. She wears the heaviest armor
-			and carries the biggest swords. She's a <span class="skill">swordsman</span> by nature, giving her a better chance to hit
-			as well as more damage with sword attacks than any other. Her strong starting armor class is more
-			than enough to recommend her.</p>
-			<h3>Wizard</h3>
-			<p>The Wizard is, as always, a total weakling physically but devastating with his spells. He
-			begins with four spells (fireball, freeze, lightning, earthquake) and a badass wooden staff. So
-			badass. He's super-smart and pretty dextrous, which makes him somewhat hard to hit. He also takes
-			half spell damage due to his knowledge of <span class="skill">necromancy</span>, which must be nice.</p>
-			<h3>Thief</h3>
-			<p>The Thief, you know, she's pretty quick. She's likely going to be the fastest and hardest
-			to hit of any character at the start. Her dual daggers offer a nice amount of finishing power and
-			her <span class="skill">stealthy</span> ways mean monsters can't find her once she's out of sight.</p>
-			<h3>Wolfman</h3>
-			<p>For being a Wolfman, he's almost never a man. He's more like a really smart wolf who looks
-			kind of mannish. Smart compared to an actual wolf, that is. He's still pretty dumb, but totally
-			ripped. He deals extra damage in melee and his <span class="skill">keenness</span> allows him to sniff out enemies behind
-			closed doors.</p>
-			<h3>Harpy</h3>
-			<p>The Harpy is much more dangerous than her stats may show. While both very wise and deeply
-			charismatic, her ability to <span class="skill">paralyze</span> the enemy with her song is literally deadly. Every
-			successful attack brings the chance of stopping any monster in its tracks for up to two turns,
-			giving her and her companions the chance to strike it down.</p>
-			<h3>Young Priest</h3>
-			<p>The Young Priest is a rollicking sort, able to deal serious damage with his maul as well as
-			act as an on-field medic. His Heal spell is relative short-ranged, but can mean the difference between
-			life and death. Like the Fighter, the Young Priest is <span class="skill">tenacious</span> and in fact could be considered
-			a dual-class character, if you care about that sort of thing.
-			<h3>Old Priest</h3>
-			<p>The Old Priest, while he still carries a formidable weapon, is more interested in the healing
-			and magical arts than straight up beating people to death. His healing spell works on every player character
-			within his sight, meaning he doesn't need to be on the frontlines to provide support, and can
-			play clean-up for weakened monsters left behind; just don't let him get overwhelmed. Like the Wizard,
-			the Old Priest only takes half spell damage due to his knowledge of <span class="skill">necromancy</span>.</p>
+		<p>There are 8 available player types: Fighter, Knight, Wizard, Thief, Wolfman, Harpy, Young
+		Priest, and Old Priest. In the descriptions I'll be mixing him/her/he/she/etc. but for all
+		characters (except for the Wolfman and Harpy) genders are randomly assigned in the game.</p>
+		
+		<p>Individual skills are highlighted in white. You probably don't know what they do, but I
+		bet you can guess in an educated way.</p>
+		<h3>Fighter</h3>
+		<p>The Fighter, as you may have guessed, likes to fight. He comes equipped with a long sword
+		and a crossbow. He is both a <span class="skill">marksman</span> and <span class="skill">tenacious</span>.
+		Marksmanship gives the
+		fighter a better chance of hitting his target with any ranged weapon; tenacity allows the
+		Fighter to overcome certain death (50% of the time) and regain hit points after what would have
+		been a fatal blow.</p>
+		<h3>Knight</h3>
+		<p>The Knight is a walking, talking (always talking) medieval tank. She wears the heaviest armor
+		and carries the biggest swords. She's a <span class="skill">swordsman</span> by nature, giving her a better chance to hit
+		as well as more damage with sword attacks than any other. Her strong starting armor class is more
+		than enough to recommend her.</p>
+		<h3>Wizard</h3>
+		<p>The Wizard is, as always, a total weakling physically but devastating with his spells. He
+		begins with four spells (fireball, freeze, lightning, earthquake) and a badass wooden staff. So
+		badass. He's super-smart and pretty dextrous, which makes him somewhat hard to hit. He also takes
+		half spell damage due to his knowledge of <span class="skill">necromancy</span>, which must be nice.</p>
+		<h3>Thief</h3>
+		<p>The Thief, you know, she's pretty quick. She's likely going to be the fastest and hardest
+		to hit of any character at the start. Her dual daggers offer a nice amount of finishing power and
+		her <span class="skill">stealthy</span> ways mean monsters can't find her once she's out of sight.</p>
+		<h3>Wolfman</h3>
+		<p>For being a Wolfman, he's almost never a man. He's more like a really smart wolf who looks
+		kind of mannish. Smart compared to an actual wolf, that is. He's still pretty dumb, but totally
+		ripped. He deals extra damage in melee and his <span class="skill">keenness</span> allows him to sniff out enemies behind
+		closed doors.</p>
+		<h3>Harpy</h3>
+		<p>The Harpy is much more dangerous than her stats may show. While both very wise and deeply
+		charismatic, her ability to <span class="skill">paralyze</span> the enemy with her song is literally deadly. Every
+		successful attack brings the chance of stopping any monster in its tracks for up to two turns,
+		giving her and her companions the chance to strike it down.</p>
+		<h3>Young Priest</h3>
+		<p>The Young Priest is a rollicking sort, able to deal serious damage with his maul as well as
+		act as an on-field medic. His Heal spell is relative short-ranged, but can mean the difference between
+		life and death. Like the Fighter, the Young Priest is <span class="skill">tenacious</span> and in fact could be considered
+		a dual-class character, if you care about that sort of thing.
+		<h3>Old Priest</h3>
+		<p>The Old Priest, while he still carries a formidable weapon, is more interested in the healing
+		and magical arts than straight up beating people to death. His healing spell works on every player character
+		within his sight, meaning he doesn't need to be on the frontlines to provide support, and can
+		play clean-up for weakened monsters left behind; just don't let him get overwhelmed. Like the Wizard,
+		the Old Priest only takes half spell damage due to his knowledge of <span class="skill">necromancy</span>.</p>
+		
 		<h2>A word about Action Points</h2>
-			<p>Every character in the game receives a number of action points per turn which they may use to
-			perform certain actions. This number is reflected in the character table which sits above the map,
-			under the "Act" column.</p>
+		<p>Every character in the game receives a number of action points per turn which they may use to
+		perform certain actions. This number is reflected in the character table which sits above the map,
+		under the "Act" column.</p>
+		
+		<p>The cost breakdown is as follows:</p>
+		
+		<ul>
+			<li>0 points: opening/closing a door, picking up an item, switching weapons, selecting
+			a spell</li>
+			<li>1 point: moving cardinally</li>
+			<li>1.5 points: moving diagonally</li>
+			<li>2 points: casting a spell, doing battle, attacking with a ranged weapon</li>
+		</ul>
 			
-			<p>The cost breakdown is as follows:</p>
-			
-			<ul>
-				<li>0 points: opening/closing a door, picking up an item, switching weapons, selecting
-				a spell</li>
-				<li>1 point: moving cardinally</li>
-				<li>1.5 points: moving diagonally</li>
-				<li>2 points: casting a spell, doing battle, attacking with a ranged weapon</li>
-			</ul>
 		<h2>The toolbar</h2>
 			<p>Down in the toolbar, below the map, sits all the action buttons. Here you'll find buttons for
 			Help (obvs), opening and closing doors, picking up items, selecting weapons to wield, spells to
-			cast, and items to use, along with the button to end your turn (hint: it says "End turn" and will
+			cast, and items to use, along with the button to end your turn (hint: it will
 			blink when your turn is over).</p>
 		<h2>Movement</h2>
-			<p>Movement is available in four (yes four!) directions. Use the up/down/left/right arrow keys
-			to move your active character around the map. If you're on a mobile device, tap the screen in the
+			<p>Movement is available in 8 directions. Use the up/down/left/right arrow keys
+			to move your active character around the map in the four cardinal directions. To move
+			diagonally, hold down a combination of an up/down and left/right arrow key to move in that
+			direction. E.g. up+right = northeast, down+left = southwest.</p>
+			
+			<p>If you're on a mobile device, tap the screen in the
 			general direction you want to go and I think you'll be pleasantly surprised. You cannot move
 			through other players or monsters.</p>
 		<h2>Doing battle</h2>
@@ -175,10 +185,27 @@
 			to use the item. Most items help regain hit points or give you extra attacks in battle. Nothing
 			nasty (yet).</p>
 			
-			<p>To pick up an item, move your character over top of the item on the map and then click the
-			"Pick up" button. The "Pick up" button only lights up when you're over top of an item, so you'll
-			know what's up. You might wonder how to tell the difference between an item and a monster; it's
-			quite simple: if you hover your mouse over a monster, its name will pop up in a little tooltip.</p>
+		<h2>Picking up and dropping</h2>
+			<p>To pick up an item/weapon/piece of armor, move your character over top of the item on the map and then click the
+			"Pick up" button (with the arrow swooping up). The "Pick up" button is only enabled when you're over top of an item, so you'll
+			know what's up. When you click the button, a list will appear which contains all of the items on
+			that square. Click the item in the list to pick it up.</p>
+			
+			<h3>Picking up armor</h3>
+			Picking up armor will cause your player to put it on and drop whatever armor
+			s/he is wearing of the same type. So if you pick up a helmet, whatever helmet you happen to be
+			wearing will be swapped for the new one and dropped on the ground.</p>
+			
+			<p>To drop an item, click the "Drop" button (with the arrown swooping down). As with the "Pick up"
+			button, a list will appear which contains all of the items you may drop.</p>
+			
+			<h3>Dropping weapons</h3>
+			<p>If you drop the weapon you're wielding, the next availabe weapon in your stash will be
+			wielded. If you drop all of your weapons, it's just you and your tiny hands against the world.</p>
+			
+			<h3>Identifying items</h3>
+			<p>You might wonder how to tell the difference between an item and a monster; it's
+			quite simple: all items appear on white tiles.</p>
 		<h2>Completing an @dventure&trade;</h2>
 			<p>After successfully completing an @dventure&trade;, you will be presented with a win screen
 			which details how great you are and how much treasure you've taken as a reward. It also lists
@@ -195,24 +222,6 @@
 			screen or by clicking the link in the passcode email you sent yourself from the win screen. In
 			both cases you must click the "Verify passcode" button below the input box in order to load up
 			your game. After that, just click "Play" to get on with your next @dventure&trade;</p>
-		<h2>Purchasing items</h2>
-			<p>After completing an @dventure&trade; or loading a saved game, before you begin your next
-			outing you will be presented with an option to purchase items for characters and equip them as
-			necessary.</p>
-			
-			<p>Each player's inventory sits on a different tab, so have a look at each player before you
-			just buy a bunch of dumb crap like in real life. All sales are final! The shop doesn't even
-			buy back for half price, that's how serious they are.</p>
-			
-			<p>To purchase, drag an item from the shop to an appropriate empty slot in your character's
-			equipment. That is, weapon slots hold weapons, helmet slots hold helmets, etc. You have to confirm
-			the purchase before it goes through so don't worry about accidentally buying something unless
-			you're really bad at things.</p>
-			
-			<p>To transfer an item from character to character, move the item first to the holding area on the right, switch tabs to
-			the receving character and then move the item from the holding area to receiving player's slot.</p>
-			
-			<p>To throw an item away, drag it to the trash box. Easy enough!</p>
 		<h2>Map features</h2>
 			<p>For the most part the maps feature items you'll be readily familiar with from real life: walls,
 			doors, water, etc. If you happen to live in a landlocked region and are perplexed upon seeing a large area
